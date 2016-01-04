17a8b93b0ed221142eb04a80c64008902754b38d
Robert Ferguson <rob@rewferguson.com>
2016-01-03 22:04:29 -0600

    project init

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+
+  gulp.watch('./scss/*.scss', ['sass']).on('change', browserSync.reload);
+  gulp.watch('./dist/index.html').on('change', browserSync.reload);
+  gulp.watch('./scripts/*.js', ['compressjs']).on('change', browserSync.reload);
+});
+
+gulp.task('sass', function() {
+  gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('compressjs', function() {
+  gulp.src('./scripts/*.js')
+  //.pipe(uglify())
+  .pipe(gulp.dest('./dist/scripts/'));
+});
+
+gulp.task('default', ['browser-sync']);
---
+++ scripts/script.js
@@ -0,0 +1,318 @@
+// game setup variables
+var FIREBASE_BASE = new Firebase('https://svgpad.firebaseio.com/');
+var firebaseRef;
+var firebasePath;
+var path;
+var pathReference;
+var firebasePathsRef;
+
+var getUrlVars = function() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    vars[key] = value;
+  });
+
+  return vars;
+};
+
+var gameId = getUrlVars()['id'];
+
+var player;
+if (Cookies.get('player')) {
+  player = Cookies.get('player');
+  Cookies.set('player', '');
+} else {
+  player = getUrlVars()['player'];
+}
+
+// word tile variables
+var wordlist = [
+  "cat",
+  "dog",
+  "bird",
+  "fish",
+  "snake",
+  "turtle",
+  "snail",
+];
+
+var title = document.getElementById('title');
+var letterPool = document.getElementById('letter-pool');
+var word = document.getElementById('word');
+var currentPosition;
+var currentLetter;
+var jumble;
+var solution;
+var checkPosition;
+var selection;
+
+// setup paper.js
+paper.install(window);
+window.canvas = document.getElementById('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+paper.setup(canvas);
+
+// jQuery ready
+$(function() {
+
+  // check for exising game ID
+  if (!gameId) {
+    $('#menu').show();
+    firebaseRef = FIREBASE_BASE.push();
+    var path = 'index.html?id=' + firebaseRef.key();
+    $('#drawBtn').attr('href', path);
+    $('#guessBtn').attr('href', path);
+  } else {
+    $('#menu').hide();
+    firebaseRef = FIREBASE_BASE.child(gameId);
+  }
+
+  // setup firebase response to path data
+  pathReference = {};
+  firebasePathsRef = firebaseRef.child('paths');
+
+  firebasePathsRef.on('child_changed', function(snapshot) {
+    var firebaseData = snapshot.val();
+    //console.log(firebaseData.path);
+    pathReference[snapshot.key()].pathData = firebaseData.path;
+    view.update();
+  });
+
+  firebasePathsRef.on('child_added', function(snapshot) {
+    var firebaseData = snapshot.val();
+    pathReference[snapshot.key()] = new Path();
+    pathReference[snapshot.key()].pathData = firebaseData.path;
+    pathReference[snapshot.key()].strokeColor = 'red';
+    pathReference[snapshot.key()].strokeWidth = 3;
+    view.update();
+  });
+
+  // select player draw
+  $('#drawBtn').click(function() {
+    var path = $(this).attr('href');
+    path += '&player=guess';
+    $(this).attr('href', path);
+    Cookies.set('player', 'draw');
+
+    // choose a word at random anyway
+    selection = wordlist[Math.floor(Math.random() * wordlist.length)];
+    firebaseRef.set({word: selection, completed: false}, function() {
+      window.location = path;
+    });
+
+    return false;
+  });
+
+  // select player guess
+  $('#guessBtn').click(function() {
+    var path = $(this).attr('href');
+    path += '&player=draw';
+    $(this).attr('href', path);
+    Cookies.set('player', 'guess');
+
+    // choose a word at random
+    selection = wordlist[Math.floor(Math.random() * wordlist.length)];
+    firebaseRef.set({word: selection, completed: false}, function() {
+      window.location = path;
+    });
+
+    return false;
+  });
+
+  firebaseRef.child('completed').on('value', function(snapshot) {
+    if (snapshot.val() === true) {
+      $('#gameOver').show();
+      paper.project.activeLayer.children = [];
+      view.update();
+      $('#word').empty();
+      $('#letter-pool').empty();
+      jumble = [];
+      if (player === 'draw') {
+        //Cookies.set('player', 'guess');
+        player = 'guess';
+      } else if (player === 'guess') {
+        //Cookies.set('player', 'draw');
+        player = 'draw';
+      }
+    } else {
+      $('#gameOver').hide();
+    }
+  });
+
+  $('#gameOver').click(function() {
+    // choose a word at random
+    selection = wordlist[Math.floor(Math.random() * wordlist.length)];
+
+    // selection = selection.split('');
+    firebaseRef.set({word: selection, completed: false});
+  });
+
+  // get current word from firebase
+  firebaseRef.child('word').on('value', function(snapshot){
+    selection = snapshot.val();
+    console.log(selection);
+    jumble = new Array();
+    solution = new Array();
+    $('#word').empty();
+    $('#letter-pool').empty();
+
+
+    // *****
+    // check for player type
+    // setup paperjs for draw player
+    // *****
+    if (player === 'draw') {
+      $('#title').html(selection);
+      $('#title').show();
+      $('#letter-pool').hide();
+      $('#word').hide();
+
+      var tool = new Tool();
+
+      tool.onMouseDown = function(event) {
+        if (player == 'draw') {
+          // If we produced a path before, deselect it:
+          if (path) {
+              path.selected = false;
+          }
+
+          // Create a new path and set its stroke color to black:
+          path = new Path({
+              segments: [event.point],
+              strokeColor: 'black',
+              // Select the path, so we can see its segment points:
+              fullySelected: true
+          });
+
+          firebasePath = firebasePathsRef.push();
+          pathReference[firebasePath.key()] = new Path();
+        }
+      }
+
+      // While the user drags the mouse, points are added to the path
+      // at the position of the mouse:
+      tool.onMouseDrag = function(event) {
+        if (player == 'draw') {
+          path.add(event.point);
+          firebasePath.set({path: path.pathData});
+        }
+      };
+
+      // When the mouse is released, we simplify the path:
+      tool.onMouseUp = function(event) {
+        if (player == 'draw') {
+          //path.simplify(10);
+          path.fullySelected = false;
+          path.remove();
+        }
+      }
+
+
+
+
+    // *****
+    // setup tiles for guess player
+    // *****
+  } else if (player === 'guess') {
+      // split the characters into an array
+      selection = selection.split('');
+
+      $('#title').hide();
+      $('#letter-pool').show();
+      $('#word').show();
+
+
+
+      // loop over the array and print out drop containers
+      // push each character to a new array that we can shuffle
+      selection.forEach(function(index) {
+        var tile = document.createElement('div');
+        tile.classList.add('position');
+        tile.setAttribute('droppable', true);
+        tile.setAttribute('data-letter', index);
+        word.appendChild(tile);
+        jumble.push(index);
+      });
+
+      // shuffle the characters in the new word array
+      jumble.sort(function() {
+        return 0.5 - Math.random();
+      });
+
+      // print out characters on screen;
+      jumble.forEach(function(index){
+        var tile = document.createElement('div');
+        var text = document.createTextNode(index);
+        tile.classList.add('letter');
+        tile.appendChild(text);
+        letterPool.appendChild(tile);
+      });
+
+      // check if the letter tile is put in the right place
+      checkPosition = function(position, letter) {
+        //console.log(position, letter);
+        if (position == letter) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+
+      // add jquery ui drop function
+      $('.position').droppable({
+        tolerance: "pointer",
+        over: function() {
+          $(this).addClass('position--hover');
+        },
+        out: function() {
+          $(this).removeClass('position--hover');
+        },
+        drop: function(event, ui) {
+          //console.log(event);
+
+          currentPosition = event.target;
+          currentPosition.classList.remove('position--hover');
+
+          if (checkPosition(currentPosition.getAttribute('data-letter'), ui.draggable.html())) {
+            currentPosition.classList.add('position--correct');
+
+            ui.draggable.css('position', 'absolute');
+            ui.draggable.css('left', '-1px');
+            ui.draggable.css('top', '-1px');
+            $(currentPosition).append(ui.draggable);
+
+            solution.push(ui.draggable.html());
+            if (solution.join() === selection.join()) {
+              //word.classList.add('word--correct');
+              firebaseRef.child('completed').set(true);
+              firebaseRef.child('paths').set([]);
+            }
+          } else {
+            currentPosition.classList.add('position--incorrect');
+            ui.draggable.animate({
+              left: 0,
+              top: 0
+            }, 150, function() {
+              currentPosition.classList.remove('position--incorrect');
+            });
+          }
+        }
+      });
+
+      // add jquery ui drag function
+      $('.letter').draggable({
+        stop: function( event, ui ) {
+          //console.log(event);
+          if (!ui.helper.parent().hasClass('position')) {
+            ui.helper.animate({
+              left: 0,
+              top: 0
+            }, 150);
+          }
+        }
+      });
+    }
+  });
+
+}); // jQuery ready
